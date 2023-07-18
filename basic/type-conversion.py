ed03c94588528e66bfdab00037fd0ad7e7bb9761
ianfleon <ianfleon08@gmail.com>
2023-07-19 02:04:52 +0900

    basic

---
+++ basic/type-conversion.py
@@ -0,0 +1,8 @@
+# Semua input mengembalikan string
+# Kita perlu mengubah tipe data jika diperlukan
+
+nama = input('Masukan nama:')
+nilai = input('Masukan nilai raport: ')
+
+print(f'Nama saya: {nama}')
+print('Nilai:' + (int(nilai) * 2))
---
+++ basic/variable.py
@@ -0,0 +1,18 @@
+# String
+nama = "Ianfleon"
+
+# Number
+angkatan = 2016
+
+# Boolean
+lajang = False
+
+print(f"Nama: {nama}")
+print(angkatan)
+print(lajang)
+
+# Python tidak mengenal Konstanta, biasanya dibuat-buat spt. mempunyai dengan cara seperti penamaan konstanta umumnya
+
+BASE_URL = 'https://ianfleon.my.id'
+print(BASE_URL)
+
