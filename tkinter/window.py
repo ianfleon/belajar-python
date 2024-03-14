6cadac4c9f9349ba08fc040b5956cc3416c0955e
devikaodeee <devikareza2622@gmail.com>
2024-03-14 12:59:19 +0800

    new tutorial

---
+++ tkinter/window.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+# Init Tkinter
+root = tk.Tk()
+
+# Set Title
+root.title("Aplikasiku")
+
+# Set Size and Window Position
+# Width x Height + X + Y
+
+root.geometry("600x200+50+100")
+
+root.mainloop()
